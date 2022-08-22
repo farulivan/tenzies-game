236f7640befb9eafdfb8e381a7dde63958c01376
Farul Ivan <farulivan@gmail.com>
2022-08-22 17:49:20 +0700

    create die component & adding state for random dice

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
-import { useState } from 'react'
+import { useState } from "react"
+import Die from "./components/Die"
 
 function App() {
-  const [count, setCount] = useState(0)
+  const allNewDice = () => {
+    const newDice = []
+
+    for (let i=0; i < 10; i++){
+      const randomNum = Math.ceil(Math.random() * 6)
+      newDice.push(randomNum)
+    }
+    
+    return newDice
+  }
+
+  const [allDice, setAllDice] = useState(allNewDice())
+
+  const diceElements = allDice.map(die => <Die value={die} />)
 
   return (
     <div className="App">
@@ -11,18 +25,9 @@ function App() {
             <h1 className="text-2xl">Tenzies</h1>
             <p className="font-inter text-sm px-5 my-1">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
           </div>
-          <section className="Main grid grid-cols-5 my-5 gap-7 p-2 text-center">
-            <div className="number bg-emerald-400 rounded-lg drop-shadow-md h-11 text-xl flex items-center justify-center">1</div>
-            <div className="number bg-slate-100 rounded-lg drop-shadow-md h-11 text-xl flex items-center justify-center">1</div>
-            <div className="number bg-emerald-400 rounded-lg drop-shadow-md h-11 text-xl flex items-center justify-center">1</div>
-            <div className="number bg-emerald-400 rounded-lg drop-shadow-md h-11 text-xl flex items-center justify-center">1</div>
-            <div className="number bg-emerald-400 rounded-lg drop-shadow-md h-11 text-xl flex items-center justify-center">1</div>
-            <div className="number bg-emerald-400 rounded-lg drop-shadow-md h-11 text-xl flex items-center justify-center">1</div>
-            <div className="number bg-emerald-400 rounded-lg drop-shadow-md h-11 text-xl flex items-center justify-center">1</div>
-            <div className="number bg-emerald-400 rounded-lg drop-shadow-md h-11 text-xl flex items-center justify-center">1</div>
-            <div className="number bg-emerald-400 rounded-lg drop-shadow-md h-11 text-xl flex items-center justify-center">1</div>
-            <div className="number bg-emerald-400 rounded-lg drop-shadow-md h-11 text-xl flex items-center justify-center">1</div>
-          </section>
+          <div className="die-container grid grid-cols-5 my-5 gap-7 p-2 text-center">
+            {diceElements}
+          </div>
           <section className="Roll flex justify-center">
             <div className="roll-button bg-cyan-700 w-24 h-12 flex justify-center items-center text-lg text-slate-100 rounded-xl drop-shadow-md">Roll</div>
           </section>
