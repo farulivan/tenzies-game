bb24a7472ceac34e5b7556ca4082bde6e9fcfcd7
Farul Ivan <farulivan@gmail.com>
2022-08-22 17:49:53 +0700

    create die comp & add state for random dice

---
+++ src/components/Die.jsx
@@ -0,0 +1,7 @@
+const Die = (props) => {
+    return (
+        <div className="number bg-emerald-400 rounded-lg drop-shadow-md h-11 text-xl flex items-center justify-center">{props.value}</div>
+    )
+}
+
+export default Die
